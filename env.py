a2309350b2376b067ee559e9134e84e48050d077
SHAM VIGNESH <shyamvignesh184.20@gmail.com>
2026-07-06 15:32:56 +0530

    Security: Untrack compiled binaries and local environment configurations

--- env.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-connection_string = (
-    "DRIVER={ODBC Driver 17 for SQL Server};"
-    "SERVER=DESKTOP-G2ETPA9;"
-    "DATABASE=RestaurantIntelligenceDB;"
-    "Trusted_Connection=yes;"
-)
-def get_db_conn():
-    return pyodbc.connect(connection_string)
